a8bf02bac859be496f5548052e199c68bef0fc74
panbaoyi <1786341231@qq.com>
2016-04-06 10:48:47 +0800

    头部导航栏动效,侧边栏内容,登陆模态框

--- app/static/js/pages/index.js
+++ app/static/js/pages/index.js
@@ -45,6 +45,47 @@ $(document).ready(function(){
         $(this).find(".bottom").removeClass('slideUp');
     })
 })
+//toggle-sidebar监听浏览器高度变化
+$(document).ready(function(){
+var scrolling;
+document.addEventListener("scroll", setScroll, false);
+document.addEventListener("mousewheel", setScroll, false);
+function setScroll(){
+    scrolling = true;
+}
+    function animateOnScroll(){
+        var requestAnimationFrame = window.requestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.msRequestAnimationFrame;
+        if (scrolling){
+            var scrollPosition = window.scrollY;
+            $(".header-bg").css({
+                transform: 'translate3d(0px,' + scrollPosition / 2 + 'px,0px)'
+            });
+            $(".header-bg-wrapper").css({
+                transform: 'translate3d(0px,' + scrollPosition / 4 + 'px,0px)'
+            })
+            if (scrollPosition>70){
+                $(".page").addClass('static').css({
+                    transform: 'translate3d(0px,' + scrollPosition + 'px,0px)'
+                })
+            }else {
+                $(".page").removeClass('static').css({
+                    transform: 'translate3d(0px,0px,0px)'
+                })
+            }
+            scrolling = false;
+        }
+        requestID = requestAnimationFrame(animateOnScroll)
+    }
+    animateOnScroll();
+})
+//uk-slidedown切换
+//$(".uk-parent").find("a").on("click",function(){
+//    $(this).next().toggleClass("uk-hidden");
+//})
+
 
 
 
